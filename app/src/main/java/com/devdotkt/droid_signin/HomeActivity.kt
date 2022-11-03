5b2603648c04693fda0f74f2615f19aefc1396bd
Bruno <bruno.lo.dev@gmail.com>
2022-11-03 20:20:47 -0300

    README: Adding images to GitHub.

--- app/src/main/java/com/devdotkt/droid_signin/HomeActivity.kt
+++ app/src/main/java/com/devdotkt/droid_signin/HomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
@@ -20,11 +21,14 @@ class HomeActivity : AppCompatActivity() {
         val email = intent.getStringExtra("email")
         val displayName = intent.getStringExtra("name")
 
-        findViewById<TextView>(R.id.accountCenterText).text = email + "\n" + displayName
+        findViewById<TextView>(R.id.accountCenterText).text = "Name: " + displayName + "\n" + "Email: " + email
 
 
         findViewById<Button>(R.id.SignOutBtn).setOnClickListener {
             auth.signOut()
+
+            FirebaseAuth.getInstance().signOut()
+
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
